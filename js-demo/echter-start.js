7beec5b25afd372da9490db5098ed50e6951c7ad
Malte Schmitz <malte@schmitz-sh.de>
2012-09-11 13:26:42 +0200

    Improves the real start.

--- js-demo/echter-start.js
+++ js-demo/echter-start.js
@@ -3,3 +3,13 @@ x = x +1;
 document.writeln(x);
 x = "Malte";
 document.writeln(x);
+
+var test = function(x) {
+	return x*x;
+}
+
+function foo(fun) {
+	return fun(3);
+}
+
+document.writeln(foo(test));
