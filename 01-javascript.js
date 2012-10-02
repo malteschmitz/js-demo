3423165ae41b39680b8e4bbb1c0565fce2949892
malteschmitz <malte@schmitz-sh.de>
2012-10-02 23:33:53 +0200

    Adds comments.

--- 01-javascript.js
+++ 01-javascript.js
@@ -1,15 +1,92 @@
+// In this file we will introduce variables, if statements, for and while loops
+// and simple functions. You might guess that this is an inline comment.
+/* This is a block comment. */
+
+// Variables do not have a fixed type in JavaScript. The are declared using
+// the var keyword. If you leave out the var keyword the variable will be
+// global.
+
+// Global variables are'nt really variables but properties of the global
+// object. In the context of a browser the global object is the window
+// object which can be accessed using its own property window. You will
+// see later that you can create a new property of an object just by assigning
+// a value to it. So
+x = 12;
+// actually means window.x=12; Lets test it
+document.writeln(window.x);
+
+// You nearly never want a variable to become global! So lets declare a
+// local variable
 var x = 12;
 x = x +1;
 document.writeln(x);
+// We use document.writeln, the writeln method of the document object (a
+// property of the windows object). This writes code directly into the HTML
+// document and can only be used in scripts which are loaded in the body.
+// Normally scripts are loaded in the head and document.write(ln) cannot
+// be used (and has strange effects).
+
+// A variable does not have a type. Only its current value has a type.
+// So you can change the type of a variable just by assigning another value
+// to it. (Of cause this is not very good programming style.)
 x = "Malte";
 document.writeln(x);
 
-var test = function(x) {
-	return x*x;
+// A variable does not have a type. Only its current value has a type.
+// So you can change the type of a variable just by assigning another value
+// to it. (Of cause this is not very good programming style.)
+x = "Malte";
+document.writeln(x);
+
+// Variable scopes are different to many other languages. A variable
+// declaration gets moved to the top of the current block (function body --
+// nothing else) implicitly. So the variable a1 is not declared here and
+// trying to read it will cause an error.
+// document.writeln(a1); // will cause an error
+// The variable a2 is declared here,
+// and has the value undefined, because it gets declared later on in this block.
+// Of cause the initialization of a2 takes place exactly where its written,
+// therefore its value is defined up here.
+document.writeln(a2); // undefined
+// many things may happen here
+var a2 = 42;
+// now it has a value
+document.writeln(a2); // 42
+
+// if statements, for and while loops look very much like c or java:
+if (x == 13) {
+  document.writeln("Hallo")
 }
 
+// this loop
+for (var i = 1; i < 5; i++) {
+  document.write(i + ', ');
+}
+document.writeln(i);
+// does the same as this one
+i = 1;
+while (i < 5) {
+  document.write(i + ', ');
+  i++;
+}
+document.writeln(i);
+
+// You can declare functions like this.
 function foo(fun) {
 	return fun(3);
 }
-
+// Actually this was just a shorthand for var foo = function(fun) { ...
+// This means functions are just values of variables like strings or objects.
+// So lets write another function
+var test = function(x) {
+	return x*x;
+}
+// and pass it as an argument to the first one.
 document.writeln(foo(test));
+
+// The number of arguments a function has is very flexible. If yuo call if
+// function with lesser arguments than declared the other arguments will
+// stay undefined. If you call a function with more arguments then declared
+// they will get ignored. (Not exacly, actually every function has an
+// arguments object that acts like an array (but is none) and contains
+// all the arguments the function gets called with.)
--- 02-functions.js
+++ 02-functions.js
@@ -1,19 +1,47 @@
-document.writeln("Hallo Welt");
-  
+// Lets write a recursive function
+function fac(i) {
+  if (i == 1) {
+    return 1;
+  }
+  return i * fac(i-1);
+};
+// Now lets compute 4! = 4*3*2*1
+document.writeln(fac(4)); // 24
+
+// Now lets generalize the idea to handle
+// \sum_{i=1}^x i instead of x! = \prod_{i=1}^x i, too. 
+// Lets create a recursive function that accepts a callback function which is
+// used to compute the multiplication in
+// i * fac(i-1)
+// or the addition in
+// i + gauss(i-1)
 function rec(callback,i) {
   if (i == 1) {
     return 1;
   }
   return callback(i, rec(callback,i-1));
 };
-
+// Now we need functions for multiplication and addition... 
 var add = function(a,b) {
   return a + b;
 };
-
 var mul = function(a,b) {
   return a * b;
 }
-
+// and can test everything
 document.writeln(rec(add,4));
 document.writeln(rec(mul,4));
+
+// PS: Yes, I know that it is much more efficient to do this:
+function efficient_fac(x) {
+  var result = 1
+  for (var i = 2; i <= x; i++) {
+    result *= i;
+  }
+  return result;
+}
+function efficient_gauss(x) {
+  return x*(x+1)/2
+}
+document.writeln(efficient_fac(4)); // 1*2*3*4=24
+document.writeln(efficient_gauss(4)); // 1+2+3+4=10
